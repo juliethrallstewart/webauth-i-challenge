33ac238a2a6ba8789c9cc2cf75b508f6a5145cb9
Julie Thrall <juliethrallstewart@gmail.com>
2019-09-17 13:51:36 -0700

    added restricted to get users

--- users/users-router.js
+++ users/users-router.js
@@ -7,38 +7,8 @@ const Users = require('./users-model.js');
 
 const router = express.Router();
 
-router.post('/register', (req, res) => {
-    let { username, password } = req.body;
-    console.log(req.body)
-    const hash = bcrypt.hashSync(password, 8); // it's 2 ^ 8, not 8 rounds
-    Users.add({ username, password: hash })
-      .then(saved => {
-        res.status(201).json(saved);
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
-  });
-  
-  router.post('/login', (req, res) => {
-    let { username, password } = req.body;
-    const user = req.body
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        console.log(user, 'logging user')
-        if (user && bcrypt.compareSync(password, user.password)) {
-          res.status(200).json({ message: `Welcome ${user.username}!` });
-        } else {
-          res.status(401).json({ message: 'You cannot pass!' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
-  });
-  
-  router.get('/', (req, res) => {
+    
+  router.get('/', restricted, (req, res) => {
     Users.find()
       .then(users => {
         res.json(users);
@@ -46,22 +16,4 @@ router.post('/register', (req, res) => {
       .catch(err => res.send(err));
   });
   
-  router.get('/hash', (req, res) => {
-    const name = req.query.name;
-  
-    // hash the name
-    const hash = bcrypt.hashSync(name, 8); // use bcryptjs to hash the name
-    res.send(`the hash for ${name} is ${hash}`);
-  });
-
-  module.exports = router;
-
-//   {
-//     "username": "chrisrobin",
-//     "password": "burgers",
-// 		"lastName": "stewart",
-// 		"firstName": "chris",
-//     "email": "legacy@gmail.com",
-//     "phone": "5555555555",
-//     "address": "3315 S 12 St, tacoma, wa 98405"
-//   }
+module.exports = router;
